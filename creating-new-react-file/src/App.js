b00e2078ad220ec7cd9a282258cc6deaf08b456c
Neha0916 <cutiepieneha789@gmail.com>
2024-03-02 13:37:54 +0530

    changes in app.js and app.css

--- creating-new-react-file/src/App.js
+++ creating-new-react-file/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name="Neha";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+      <h1>Hello  {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque ex doloribus magnam tempore at, odio quos eligendi deleniti ipsum alias numquam itaque corrupti consequuntur quo possimus quis ad voluptate. Modi impedit quia quasi laudantium at fuga iusto officiis aliquid repellendus.</p>
+      </div>
+    </>
   );
 }
 
