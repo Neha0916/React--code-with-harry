153a90bee5c2d8714ec08f129302bce191acb713
Neha0916 <cutiepieneha789@gmail.com>
2024-03-04 23:33:43 +0530

    useState and State used

--- creating-new-react-file/src/App.js
+++ creating-new-react-file/src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 function App() {
   return (
     <>
 <Navbar title="TextUtils" aboutText="About TextUtils"/>
+<div className="container my-3">
+<TextForm heading="Enter the content to analyze below" />
+</div>
     </>
   ); 
 }
