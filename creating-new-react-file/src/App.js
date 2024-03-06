cb7fd906b02f88df4d443e51e6ed1f7bf6c8fd9a
Neha0916 <cutiepieneha789@gmail.com>
2024-03-06 20:04:14 +0530

    alert used and added it to textutils

--- creating-new-react-file/src/App.js
+++ creating-new-react-file/src/App.js
@@ -2,30 +2,46 @@
 import './App.css';
 // import About from './Components/About';
 import Navbar from './Components/Navbar';
+import Alert from './Components/Alert';
 import TextForm from './Components/TextForm';
 import React,{useState} from 'react'
 
 function App() {
   const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg: message,
+      type:type
+    })
+    setTimeout(()=>{
+        setAlert(null);
+    },2000)
+  }
 
   const toggleMode=()=>{
     console.log(mode);
     if(mode==='dark'){
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
     }
     else{
       setMode('dark');
       document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
     }
   }
 
   return (
     <>
     {/* console.log({mode}); */}
+    <Alert alert={alert}/>
 <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-<TextForm heading="Enter the content to analyze below" mode={mode} />
+<TextForm heading="Enter the content to analyze below" mode={mode} showAlert={showAlert} />
+
 </div>
 {/* <div className="container">
   <About/>
--- creating-new-react-file/src/Components/TextForm.js
+++ creating-new-react-file/src/Components/TextForm.js
@@ -4,35 +4,40 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick=()=>{
-        console.log("To Upper Case" + text);
+        // console.log("To Upper Case" + text);
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Convertd to UpperCase","success");
     }
     const handleDownClick=()=>{
-        console.log("To Lower Case" + text);
+        // console.log("To Lower Case" + text);
         let newText=text.toLowerCase();
         setText(newText);
+        props.showAlert("Convertd to LowerCase","success");
     }
     const handleOnChange=(event)=>{
         
         setText(event.target.value);
+
     }
     const displayTrimClick=()=>{
         let newText=text.split(/[ ]+/);
         newText=newText.join(" ");
         setText(newText);
+        props.showAlert("Removed all the extra spaces","success");
     }
     const displayClearClick=()=>{
         const newText='';
         setText(newText);
+        props.showAlert("Text Box is all Cleared","success");
     }
-    const[text, setText] =useState('')
+    const[text, setText] =useState('');
   return (
     <>
     <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
 <div className="mb-3" style={{color: props.mode==='light'?'black':'white'}}>
 <h1 >{props.heading}</h1>
-    <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'#042743':'white',color: props.mode==='light'?'black':'white'}} id="myBox" value="Enter text here" onChange={handleOnChange} rows="8" ></textarea>
+    <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'#042743':'white',color: props.mode==='light'?'black':'white'}} id="myBox"  value={text} onChange={handleOnChange} rows="8" ></textarea>
 </div>
 <button className="btn btn-success mx-3 my-3" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-success mx-3 my-3" onClick={handleDownClick}>Convert to Lowercase</button>
