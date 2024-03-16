10974160fb76ba6dbc3f1decd002178c0de7c5a1
Neha0916 <cutiepieneha789@gmail.com>
2024-03-16 15:08:38 +0530

    used prev and next buttons to move the pages

--- newsapp/news-app/src/components/News.js
+++ newsapp/news-app/src/components/News.js
@@ -7,21 +7,54 @@ export default class News extends Component {
     // console.log("Hello I am a constructor");
     this.state={
       articles:[],
-      loading:true
+      loading:true,
+      page:1
     }
   }
 
 async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=067b6b09407344599f58f8a0d85fd9a3";
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=067b6b09407344599f58f8a0d85fd9a3&page=1&pageSize=20";
+    let data=await fetch(url);
+    let parseData=await data.json();
+    console.log(parseData);
+    this.setState({articles:parseData.articles,totalResults:parseData.totalResults
+    })
+}
+
+handlePrevClick=async()=>{
+  
+  let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=067b6b09407344599f58f8a0d85fd9a3&page=${this.state.page-1}&pageSize=20`;
+    let data=await fetch(url);
+    let parseData=await data.json();
+    console.log(parseData);
+  this.setState({
+    articles:parseData.articles,
+    page:this.state.page-1,
+  })
+  // console.log(url)
+}
+handleNextClick = async()=>{
+  if(this.state.page+1>Math.ceil(this.state.totalResults/20)){
+  
+}else{
+  let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=067b6b09407344599f58f8a0d85fd9a3&page=${this.state.page+1}&pageSize=20`;
     let data=await fetch(url);
     let parseData=await data.json()
     console.log(parseData);
-    this.setState({articles:parseData.articles})
+    this.setState({
+      articles:parseData.articles,
+    page:this.state.page+1,
+  })
+  // console.log(url)
 }
+}
+
+
 
   render() {
     return (
-      <div className='container my-3'>
+      <>
+        <div className='container my-3'>
         <h2>NewsMonkey - Top Headlines</h2>
         
         <div className="row ">
@@ -32,6 +65,11 @@ async componentDidMount(){
         })}   
         </div>
       </div>
+      <div className="container my-3 d-flex justify-content-between">
+      <button disabled={this.state.page===1} type="button" className="btn btn-danger" onClick={this.handlePrevClick}>&larr; Prev</button>
+      <button type="button" className="btn btn-danger" onClick={this.handleNextClick}>Next &rarr;</button>
+      </div>
+      </>
     )
   }
 }
