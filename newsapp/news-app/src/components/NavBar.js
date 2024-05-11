5ffbbf95ee9aa4e3123a2a4a3f2f8971d01761ff
Neha0916 <cutiepieneha789@gmail.com>
2024-05-11 06:40:48 +0530

    Update NavBar.js

--- newsapp/news-app/src/components/NavBar.js
+++ newsapp/news-app/src/components/NavBar.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NavBar extends Component {
+const NavBar=()=> {
 
-  render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsMonkey</a>
+    <a className="navbar-brand" href="/">NewsHub</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -48,6 +47,5 @@ export class NavBar extends Component {
       </div>
     )
   }
-}
 
 export default NavBar
