7bb5bfab3f878fc6c4028e44ee0df322d7b5bd5d
Neha0916 <cutiepieneha789@gmail.com>
2024-05-11 06:41:00 +0530

    Update Spin.js

--- newsapp/news-app/src/components/Spin.js
+++ newsapp/news-app/src/components/Spin.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loading from './loading.gif'
-export default class Spin extends Component {
-  render() {
+const Spin=()=> {
+  
     return (
       <div className='text-center'>
         <img src={loading} alt="loading" />
       </div>
     )
-  }
 }
+
+export default Spin
